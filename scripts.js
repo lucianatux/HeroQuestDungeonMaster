ac3d8fa6f18c0dc962ba4264fd35dd22b9d09c2f
lucianatux <lucianatux@yahoo.com.ar>
2023-10-29 21:15:40 -0300

    setupmonsters

--- scripts.js
+++ scripts.js
@@ -33,35 +33,59 @@ const tohome = document.querySelector('#tohome');
 const missions = {
     1: {
         title: "Misión 1: El Comienzo",
-        purpose: "Tu primera misión es enfrentarte a las ratas gigantes que han invadido el sótano del reino. Debes limpiar el sótano y garantizar la seguridad de todos los aldeanos."
+        purpose: "Tu primera misión es enfrentarte a las ratas gigantes que han invadido el sótano del reino. Debes limpiar el sótano y garantizar la seguridad de todos los aldeanos.",
+        note1: "note1",
+        note2: "note2",
+        note3: "note3"
     },
     2: {
         title: "Misión 2: Título: La Caza del Dragón",
-        purpose:" En lo más profundo de las Montañas de Fuego, los rumores de un dragón aterrador han llevado al reino al borde del pánico. La amenaza que representa es inmensa, y no puedes permitir que continúe amenazando a tus compatriotas. Debes adentrarte en el corazón de las montañas, encontrar la guarida del dragón y enfrentarte a la bestia. Esta es una misión en la que te arriesgarás todo, pero solo tú puedes proteger el reino de su furia. La gloria y la recompensa esperan a aquel que sobreviva." 
+        purpose:" En lo más profundo de las Montañas de Fuego, los rumores de un dragón aterrador han llevado al reino al borde del pánico. La amenaza que representa es inmensa, y no puedes permitir que continúe amenazando a tus compatriotas. Debes adentrarte en el corazón de las montañas, encontrar la guarida del dragón y enfrentarte a la bestia. Esta es una misión en la que te arriesgarás todo, pero solo tú puedes proteger el reino de su furia. La gloria y la recompensa esperan a aquel que sobreviva." ,
+        note1: "note1",
+        note2: "note2",
+        note3: "note3"
     },
     3: {
         title: "mision 3",
-        purpose:" purpose 3"
+        purpose:" purpose 3",
+        note1: "note1",
+        note2: "note2",
+        note3: "note3"
     },
     4: {
         title: "mision 4",
-        purpose:" purpose 4"
+        purpose:" purpose 4",
+        note1: "note1",
+        note2: "note2",
+        note3: "note3"
     },
     5: {
         title: "mision 5",
-        purpose:" purpose 5"
+        purpose:" purpose 5",
+        note1: "note1",
+        note2: "note2",
+        note3: "note3"
     },
     6: {
         title: "mision 6",
-        purpose:" purpose 6"
+        purpose:" purpose 6",
+        note1: "note1",
+        note2: "note2",
+        note3: "note3"
     },
     7: {
         title: "mision 7",
-        purpose:" purpose 7"
+        purpose:" purpose 7",
+        note1: "note1",
+        note2: "note2",
+        note3: "note3"
     },
     8: {
         title: "mision 8",
-        purpose:" purpose 8"
+        purpose:" purpose 8",
+        note1: "note1",
+        note2: "note2",
+        note3: "note3"
     }  
 };
 
@@ -72,8 +96,8 @@ const furnitures = [
 
 const monsters = {
     goblin: 1,
-    orc: 2,
     skeleton: 1, 
+    orc: 2,
     zombie: 2,
     mummie: 3,
     abomination: 3,
@@ -138,3 +162,42 @@ setupRoomTransition(toroomnine, roomeight, roomnine);
 setupRoomTransition(toroomten, roomnine, roomten);
 setupRoomTransition(tofinal, roomten, final);
 setupRoomTransition(tohome, final, screenDiv);
+
+
+function setupMonsters(playerLevel, monsters) {
+    const monsterNames = Object.keys(monsters);
+    console.log(monsterNames);
+    const generatedMonsters = [];
+    let numMonsters = 0;
+    let probabilities;
+    if (playerLevel === 1) {
+        numMonsters = Math.floor(Math.random() * 4);
+        probabilities = [0.25, 0.25, 0.15, 0.15, 0.07, 0.07, 0.04, 0.01, 0.01];
+    } else if (playerLevel === 2) {
+        numMonsters = Math.floor(Math.random() * 5);
+        probabilities = [0.20, 0.20, 0.18, 0.18, 0.09, 0.09, 0.04, 0.01, 0.01];
+    } else if (playerLevel === 3) {
+        numMonsters = Math.floor(Math.random() * 5);
+        probabilities = [0.18, 0.18, 0.15, 0.15, 0.11, 0.11, 0.1, 0.01, 0.01];
+    } else if (playerLevel === 4) {
+        numMonsters = Math.floor(Math.random() * 6);
+        probabilities = [0.14, 0.14, 0.15, 0.15, 0.15, 0.15, 0.1, 0.01, 0.01];
+    }
+    for (let i = 0; i < numMonsters; i++) {
+        let rand = Math.random();
+        let cumulativeProb = 0;
+        for (let j = 0; j < probabilities.length; j++) {
+            cumulativeProb += probabilities[j];
+            if (rand < cumulativeProb) {
+                generatedMonsters.push(monsterNames[j]);
+                break;
+            }
+        }
+    }
+    return generatedMonsters;
+}
+
+// Ejemplo de uso:
+const playerLevel = 4;
+const generatedMonsters = setupMonsters(playerLevel, monsters);
+console.log(generatedMonsters); // Devolverá un arreglo con nombres de monstruos generados.
